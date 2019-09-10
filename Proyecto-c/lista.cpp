81421c34288d7df41be34e9c2a56de69e37e4d86
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-10 18:48:52 -0300

    añadido el cpp de lista

---
+++ Proyecto-c/lista.cpp
@@ -0,0 +1,65 @@
+#include "lista.h"
+
+lista::lista()
+{
+    //ctor
+}
+void crear_lista(tLista* l)
+{
+
+}
+
+void l_insertar(tLista l, tPosicion p, tElemento e)
+{
+
+}
+
+void l_eliminar(tLista l, tPosicion p, void(* fEliminar)(tElemento))
+{
+
+}
+
+void l_destruir(tLista* l, void(* fEliminar)(tElemento))
+{
+
+}
+
+tElemento l_recuperar(tLista l, tPosicion p)
+{
+
+}
+
+tPosicion l_primera(tLista l)
+{
+
+}
+
+tPosicion l_siguiente(tLista l, tPosicion p)
+{
+
+}
+
+tPosicion l_anterior(tLista l, tPosicion p)
+{
+
+}
+
+tPosicion l_ultima(tLista l)
+{
+
+}
+
+tPosicion l_fin(tLista l)
+{
+
+}
+
+int l_longitud(tLista l)
+{
+
+}
+
+lista::~lista()
+{
+    //dtor
+}
--- Proyecto-c/lista.h
+++ Proyecto-c/lista.h
@@ -17,62 +17,62 @@ typedef struct celda * tPosicion;
 typedef void * tElemento;
 
 /**
- Inicializa una lista vacía.
+ Inicializa una lista vacï¿½a.
  Una referencia a la lista creada es referenciada en *L.
 **/
 extern void crear_lista(tLista * l);
 
 /**
- Inserta el elemento E, en la posición P, en L.
- Con L = A,B,C,D y la posición P direccionando C, luego:
+ Inserta el elemento E, en la posiciï¿½n P, en L.
+ Con L = A,B,C,D y la posiciï¿½n P direccionando C, luego:
  L' = A,B,E,C,D
 **/
 extern void l_insertar(tLista l, tPosicion p, tElemento e);
 
 /**
- Elimina la celda P de L. El elemento almacenado en la posición P es eliminado mediante la función fEliminar parametrizada.
+ Elimina la celda P de L. El elemento almacenado en la posiciï¿½n P es eliminado mediante la funciï¿½n fEliminar parametrizada.
  Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
 **/
 extern void l_eliminar(tLista l, tPosicion p, void (*fEliminar)(tElemento));
 
 /**
- Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la función fEliminar parametrizada.
+ Destruye la lista L, elimininando cada una de sus celdas. Los elementos almacenados en las celdas son eliminados mediante la funciï¿½n fEliminar parametrizada.
 **/
 extern void l_destruir(tLista * l, void (*fEliminar)(tElemento));
 
  /**
- Recupera y retorna el elemento en la posición P.
+ Recupera y retorna el elemento en la posiciï¿½n P.
  Si P es fin(L), finaliza indicando LST_POSICION_INVALIDA.
 **/
 extern tElemento l_recuperar(tLista l, tPosicion p);
 
 /**
- Recupera y retorna la primera posición de L.
- Si L es vacía, primera(L) = ultima(L) = fin(L).
+ Recupera y retorna la primera posiciï¿½n de L.
+ Si L es vacï¿½a, primera(L) = ultima(L) = fin(L).
 **/
 extern tPosicion l_primera(tLista l);
 
 /**
- Recupera y retorna la posición siguiente a P en L.
+ Recupera y retorna la posiciï¿½n siguiente a P en L.
  Si P es fin(L), finaliza indicando LST_NO_EXISTE_SIGUIENTE.
 **/
 extern tPosicion l_siguiente(tLista l, tPosicion p);
 
 /**
- Recupera y retorna la posición anterior a P en L.
+ Recupera y retorna la posiciï¿½n anterior a P en L.
  Si P es primera(L), finaliza indicando LST_NO_EXISTE_ANTERIOR.
 **/
 extern tPosicion l_anterior(tLista l, tPosicion p);
 
  /**
- Recupera y retorna la última posición de L.
- Si L es vacía, primera(L) = ultima(L) = fin(L).
+ Recupera y retorna la ï¿½ltima posiciï¿½n de L.
+ Si L es vacï¿½a, primera(L) = ultima(L) = fin(L).
 **/
 extern tPosicion l_ultima(tLista l);
 
  /**
- Recupera y retorna la posición fin de L.
- Si L es vacía, primera(L) = ultima(L) = fin(L).
+ Recupera y retorna la posiciï¿½n fin de L.
+ Si L es vacï¿½a, primera(L) = ultima(L) = fin(L).
 **/
 extern tPosicion l_fin(tLista l);
 
