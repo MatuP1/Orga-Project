10b6b860d3ec209f53ddd5b1e1b7b2be321dbd6f
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-16 08:46:10 -0300

    no se que hize mal pero algo hize mal

--- Proyecto-c/TestLista.c
+++ Proyecto-c/TestLista.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "LISTA.h"
+#include "felim.h"
+
+void *p;
+void fEliminar(void (*p));
 
 int main ()
 {
-    tLista* lista_prueba=NULL;
-    crear_lista(lista_prueba);
+    tLista* lista_prueba=NULL;///inisializado para prevenir el warning
+    crear_lista(lista_prueba);///asocia lista_prueba a una celda centinela.
     ///Prueba de la funciones con lista vacia.
 
+    l_eliminar((struct celda*)lista_prueba,(struct celda*)lista_prueba, fEliminar);///intenta eliminar el centinela, deberia devolver error.
 
     return 0;
 }
