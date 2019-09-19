eb8c1f88f70907f3dee3a83b48d9dbc8d5fe1de0
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-19 19:16:59 -0300

    añadido arbol.c y arbol.h

---
+++ Proyecto-c/arbol.c
@@ -0,0 +1,46 @@
+#include "lista.h"
+
+void crear_arbol(tArbol* a)
+{
+
+}
+
+void crear_raiz(tArbol a, tElemento e)
+{
+
+}
+
+tNodo a_insertar(tArbol a, tNodo np, tNodo nh, tElemento e)
+{
+
+}
+
+void a_eliminar(tArbol a, tNodo pa, void(* fEliminar)(tElemento))
+{
+
+}
+
+void a_destruir(tArbol* a, void(* fEliminar)(tElemento))
+{
+
+}
+
+tElemento a_recuperar(tArbol a, tNodo n)
+{
+
+}
+
+tNodo a_raiz(tArbol a)
+{
+
+}
+
+tLista a_hijos(tArbol a, tNodo n)
+{
+
+}
+
+void a_sub_arbol(tArbol a, tNodo n, tArbol* sa)
+{
+
+}
