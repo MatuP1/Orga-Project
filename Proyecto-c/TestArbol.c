16c4cf98d4eb3b0e4cfbbbf9ead23a4654210725
IVChristos <anonymousvsworld@users.noreply.github.com>
2019-09-25 20:32:19 -0300

    empezado el tester
    solamente el mostrar lista en preorden y el fEliminar.

---
+++ Proyecto-c/TestArbol.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "arbol.h"
+#include "lista.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**Muestra el arbol en preorden.
+
+*/
+void mostarArbol(tArbol arbol){
+
+    tNodo cursor = a_raiz(arbol);
+
+    preOrden(arbol,cursor);
+
+}
+
+/**Recorrido en preOrden.
+*/
+void preOrden(tArbol arbol,tNodo cursor){
+
+    int *valor = (int *)a_recuperar(arbol,cursor);
+    printf("[%i] ", *valor);
+    tNodo nodoEnListaHijo = l_primera((cursor->hijos));
+    tNodo finListaHijo = l_fin(cursor->hijos);
+    while(cursor != finListaHijo){
+        preOrden(arbol,nodoEnListaHijo);
+    }
+
+
+}
