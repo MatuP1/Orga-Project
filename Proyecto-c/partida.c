bb31ed5c5c96094e4ea2db6e5db058ad3a390034
Propibe <matiaspippig@gmail.com>
2019-10-13 09:28:51 -0300

    Creo que mi pc flasho pero mejor que sobre que falte

--- Proyecto-c/partida.c
+++ Proyecto-c/partida.c
@@ -14,7 +14,8 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
 
     //seteo modo y turno
     (*p)->modo_partida=modo_partida;
-    (*p)->turno_de=comienza;
+    (*p)->turno_de=comienza;       //si es jugador random que?
+    (*p)->estado=PART_EN_JUEGO;
 
     //seteo nombres sin chequeo
     char guarda='\0';
@@ -33,12 +34,28 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
     (*p)->tablero=(tTablero) malloc (sizeof(struct tablero));
     if((*p)->tablero == NULL)
         exit(PART_ERROR_MEMORIA);
+    for(int i=0; i<=2; i++){
+        for(int j=0; j<=2; j++){
+            (*p)->tablero->grilla[i][j]=0;
+        }
+    }
 }
 
 int nuevo_movimiento(tPartida p, int mov_x, int mov_y){
-    return 0;
+    tTablero t=p->tablero;
+    if(t->grilla[mov_x][mov_y]==1)
+        return (PART_MOVIMIENTO_ERROR);
+    else
+        t->grilla[mov_x][mov_y]=1;
+    return PART_MOVIMIENTO_OK;      //necesita mas comtroles? cuando es necesario cambiar el estado de la partida?
+                                    //debo recorrer todoel tablero cada vez?
 }
 
 void finalizar_partida(tPartida * p){
-
+    free((*p)->nombre_jugador_1);
+    free((*p)->nombre_jugador_2);
+    free((*p)->tablero);
+    free(*p);
+    free(p);
+    p=NULL;
 }
