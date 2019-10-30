e0d6d5c60e7b2fc0d5eddcfc6627388fdd1f82f4
Propibe <matiaspippig@gmail.com>
2019-10-30 02:37:11 -0300

    Comente partida y solucione un bug
    Bug: modo usario vs maquina no jugaba si la maquina jugaba primero

--- Proyecto-c/ProgramaPrincipal.c
+++ Proyecto-c/ProgramaPrincipal.c
@@ -13,40 +13,48 @@ void printActualGame(tTablero tablero);
 
 int main(){
 
-    //Declaracion de variables.
+    ///Declaracion de variables.
     tPartida part = (tPartida)malloc(sizeof(struct partida));
     tTablero tab = (tTablero)malloc(sizeof(struct tablero));
+    ///Declaro todas las cosas de una partida
     char nombreJugador1[50];
     char nombreJugador2[50];
     int turno=0;
     int modo=0;
+    ///banderas
     int buenModo = 0;
     int estadoPartida=111;
+    ///posiciones
     int coordenadaX;
     int coordenadaY;
+    ///alojo la memoria necesaria para una busqueda adversaria
     tBusquedaAdversaria maqBusqueda = (tBusquedaAdversaria)malloc(sizeof(struct busqueda_adversaria));
 
-
-
+    ///escaneo el modo de juego
     while(!buenModo){
     printf("Indique el modo de partida\n1.Jugador vs Jugador\n2.Jugador vs Maquina\n3.Maquina vs Maquina\n");
     scanf("%i",&modo);
 
+    ///msj de error si el modo de juego no es correcto
     if(modo!=1 && modo!=2 && modo!=3){
         printf("No selecciono un modo correcto de partida\n");
     }
+
     else
         buenModo = 1;
     }
-    ///Modo usuario vs usuario.
-            printf("Indique nombre de Jugador 1.\n");
-            scanf("%s",nombreJugador1);
+    ///Escaneo el nombre del jugador 1
+    printf("Indique nombre de Jugador 1.\n");
+    scanf("%s",nombreJugador1);
+    ///Modo usario vs usario
     if(modo==1){
+            ///Guardo nombre jugador 2
             printf("Indique nombre de Jugador 2.\n");
             scanf("%s",nombreJugador2);
     }
     ///Modo usuario vs maquina
     if(modo==2){
+        ///guardo nombre de la maquina
             nombreJugador2[0] = 'M';
             nombreJugador2[1] = 'a';
             nombreJugador2[2] = 'q';
@@ -57,6 +65,7 @@ int main(){
     }
     ///Modo maquina vs maquina.
     if(modo==3){
+        //seteo nombre de ambas maquinas
             nombreJugador1[0] = 'M';
             nombreJugador1[1] = 'a';
             nombreJugador1[2] = 'q';
@@ -76,139 +85,153 @@ int main(){
             nombreJugador2[7] = ' ';
             nombreJugador2[8] = '2';
     }
-            printf("Indique '1' si empieza el jugador 1, '2' si empieza el jugador 2 o '3' para seleccion aleatoria.\n");
-            scanf("%i",&turno);
-            printf("los movimientos validos son entre 1 y 3 tanto para filas y columnas\n");
-            if(turno==1)
-                nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_1,nombreJugador1,nombreJugador2);
-            if(turno==2)
-                nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_2,nombreJugador1,nombreJugador2);
-            if(turno==3)
-                nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_RANDOM,nombreJugador1,nombreJugador2);
 
-            ///Muestro el tablero vacio.
-            tab = part->tablero;
+    ///escaneo el turno del jugador
+    printf("Indique '1' si empieza el jugador 1, '2' si empieza el jugador 2 o '3' para seleccion aleatoria.\n");
+    scanf("%i",&turno);
+    printf("los movimientos validos son entre 1 y 3 tanto para filas y columnas\n");
+    ///Dependiendo el turno inicio un tipo de partida
+    if(turno==1)
+        nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_1,nombreJugador1,nombreJugador2);
+    if(turno==2)
+        nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_2,nombreJugador1,nombreJugador2);
+    if(turno==3)
+        nueva_partida(&part,PART_MODO_USUARIO_VS_USUARIO,PART_JUGADOR_RANDOM,nombreJugador1,nombreJugador2);
 
-            printActualGame(tab);
+    ///Muestro el tablero vacio.
+    tab = part->tablero;
 
-            if(modo==1){
-                    ///Empieza la partida y no termina hasta que gana el jugador 1 el 2 o empatan.
-                while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
-                        buenModo=0;
+    printActualGame(tab);
+    printf("\n");
+    ///Modo usario vs usuario
+    if(modo==1){
+        ///Empieza la partida y no termina hasta que gana el jugador 1, el 2 o empatan.
+        while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
+                buenModo=0;
 
-                        ///Turno Jugador 1.
-                        if(part->turno_de == PART_JUGADOR_1)
-                                printf("Jugador:%s Indique su movimiento\n",nombreJugador1);
-                        else
-                                printf("Jugador:%s Indique su movimiento\n",nombreJugador2);
+                ///Turno Jugador 1.
+                if(part->turno_de == PART_JUGADOR_1)
+                        printf("Jugador:%s Indique su movimiento\n",nombreJugador1);
+                else
+                        printf("Jugador:%s Indique su movimiento\n",nombreJugador2);
 
-                        while(!buenModo){
-                            printf("Fila: ");
-                            scanf("%i",&coordenadaX);
-                            printf("Columna: ");
-                            scanf("%i",&coordenadaY);
+                while(!buenModo){
+                    ///escaneo la fila
+                    printf("Fila: ");
+                    scanf("%i",&coordenadaX);
+                    ///escaneo la columna
+                    printf("Columna: ");
+                    scanf("%i",&coordenadaY);
 
-                            ///Chequeo si son coordenadas validas.
-                            if((coordenadaX!=3 && coordenadaX!=1 && coordenadaX!=2 )|| (coordenadaY!=3 && coordenadaY!=1 &&coordenadaY!=2)){
-                                printf("El movimiento no es valido\n");
-                            }
+                    ///Chequeo si son coordenadas validas.
+                    if((coordenadaX!=3 && coordenadaX!=1 && coordenadaX!=2 )|| (coordenadaY!=3 && coordenadaY!=1 &&coordenadaY!=2)){
+                        printf("El movimiento no es valido\n");
+                    }
+                    ///si no salgo del while
+                    else{
+                        buenModo=1;
+                    }
+                }
+                ///Actualizo el estado de la partida actual con un nuevo movimiento a las coordenadas
+                estadoPartida = nuevo_movimiento(part,coordenadaX-1,coordenadaY-1);
+                ///msg de error
+                if(estadoPartida==PART_MOVIMIENTO_ERROR)
+                    printf("Intenta en un lugar que no este ocupado\n");
+                printActualGame(tab);
+                printf("\n");
+        }
+    }
+    ///Modo usario vs maquina
+    if(modo==2){
+        ///Empieza la partida y no termina hasta que gana el jugador 1 el 2 o empatan.
+        while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
+                buenModo=0;
+                 ///Turno Jugador 1.
+                if(part->turno_de == PART_JUGADOR_1){
+                    while(!buenModo){
+                        ///escaneo fila
+                        printf("Indique su movimiento\n");
+                        printf("Fila: ");
+                        scanf("%i",&coordenadaX);
+                        ///escaneo columna
+                        printf("Columna: ");
+                        scanf("%i",&coordenadaY);
 
-                            else{
-                                buenModo=1;
-                            }
+                        ///Chequeo si son coordenadas validas.
+                        if((coordenadaX!=3 && coordenadaX!=1 && coordenadaX!=2 )|| (coordenadaY!=3 && coordenadaY!=1 &&coordenadaY!=2)){
+                            printf("El movimiento no es valido\n");
                         }
-                        estadoPartida = nuevo_movimiento(part,coordenadaX-1,coordenadaY-1);
-                        if(estadoPartida==PART_MOVIMIENTO_ERROR)
-                            printf("Intenta en un lugar que no este ocupado\n");
+                        ///si no salgo del while
+                        else{
+                            buenModo=1;
+                        }
+                    }
+                    ///Realizo el movimiento.
+                    estadoPartida = nuevo_movimiento(part,coordenadaX-1,coordenadaY-1);
+                    ///msg de ERROR
+                    if(estadoPartida==PART_MOVIMIENTO_ERROR)
+                        printf("Intenta en un lugar que no este ocupado\n");
+                    printActualGame(tab);
+                    printf("\n");
+
+                }
+                ///Turno Maquina.
+                ///si el jugador 1 no termino la partida
+                if(part->turno_de == PART_JUGADOR_2 && (estadoPartida==PART_MOVIMIENTO_OK || estadoPartida==PART_EN_JUEGO)){
+                        printf("Movimiento de la maquina\n");
+                        ///Creo el arbol de busqueda adversaria.
+                        crear_busqueda_adversaria(&maqBusqueda,part);
+                        ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
+                        proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
+                        estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
+                        ///Destruyo la busqueda
+                        destruir_busqueda_adversaria(&maqBusqueda);
                         printActualGame(tab);
                 }
+        }
+    }
+
+    if(modo==3){
+
+        while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
+            ///turno maquina 1
+            if(part->turno_de == PART_JUGADOR_1){
+                ///Creo el arbol de busqueda adversaria.
+                crear_busqueda_adversaria(&maqBusqueda,part);
+                ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
+                proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
+                estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
+                destruir_busqueda_adversaria(&maqBusqueda);
+                printActualGame(tab);
             }
-
-            if(modo==2){
-                ///Empieza la partida y no termina hasta que gana el jugador 1 el 2 o empatan.
-                while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
-
-                        buenModo=0;
-
-                         ///Turno Jugador 1.
-                        if(part->turno_de == PART_JUGADOR_1){
-                            while(!buenModo){
-                                printf("Indique su movimiento\n");
-                                printf("Fila: ");
-                                scanf("%i",&coordenadaX);
-                                printf("Columna: ");
-                                scanf("%i",&coordenadaY);
-
-                                ///Chequeo si son coordenadas validas.
-                                if((coordenadaX!=3 && coordenadaX!=1 && coordenadaX!=2 )|| (coordenadaY!=3 && coordenadaY!=1 &&coordenadaY!=2)){
-                                    printf("El movimiento no es valido\n");
-                                }
-                                else{
-                                    buenModo=1;
-                                }
-                            }
-                            ///Realizo el movimiento.
-                            estadoPartida = nuevo_movimiento(part,coordenadaX-1,coordenadaY-1);
-                            if(estadoPartida==PART_MOVIMIENTO_ERROR)
-                                printf("Intenta en un lugar que no este ocupado\n");
-                            printActualGame(tab);
-
-                        }
-                        ///Turno Maquina.
-
-                        if(part->turno_de == PART_JUGADOR_2 && estadoPartida==PART_MOVIMIENTO_OK){
-                                ///Creo el arbol de busqueda adversaria.
-                                crear_busqueda_adversaria(&maqBusqueda,part);
-                                ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
-                                proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
-                                estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
-                                destruir_busqueda_adversaria(&maqBusqueda);
-                                printActualGame(tab);
-                        }
-                }
+            ///turno maquina 2
+            if(part->turno_de == PART_JUGADOR_2 && estadoPartida==PART_MOVIMIENTO_OK){
+                ///Creo el arbol de busqueda adversaria.
+                crear_busqueda_adversaria(&maqBusqueda,part);
+                ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
+                proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
+                estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
+                destruir_busqueda_adversaria(&maqBusqueda);
+                printActualGame(tab);
             }
-
-            if(modo==3){
-
-                while(estadoPartida != PART_GANA_JUGADOR_1 && estadoPartida != PART_GANA_JUGADOR_2 && estadoPartida !=PART_EMPATE){
-
-                    if(part->turno_de == PART_JUGADOR_1){
-                                    ///Creo el arbol de busqueda adversaria.
-                                    crear_busqueda_adversaria(&maqBusqueda,part);
-                                    ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
-                                    proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
-                                    estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
-                                    destruir_busqueda_adversaria(&maqBusqueda);
-                                    printActualGame(tab);
-                            }
-
-                    if(part->turno_de == PART_JUGADOR_2 && estadoPartida==PART_MOVIMIENTO_OK){
-                                    ///Creo el arbol de busqueda adversaria.
-                                    crear_busqueda_adversaria(&maqBusqueda,part);
-                                    ///Le pido el proximo movimiento a la maquina y realizo el movimiento.
-                                    proximo_movimiento(maqBusqueda,&coordenadaX,&coordenadaY);
-                                    estadoPartida=nuevo_movimiento(part,coordenadaX,coordenadaY);
-                                    destruir_busqueda_adversaria(&maqBusqueda);
-                                    printActualGame(tab);
-                            }
-                    }
-            }
-            ///Muestro el resultado de la partida.
-            if(estadoPartida == PART_GANA_JUGADOR_1){
-                printf("Gano %s!!",nombreJugador1);
-            }
-            if(estadoPartida == PART_GANA_JUGADOR_2){
-                printf("Gano %s!!",nombreJugador2);
-            }
-            if(estadoPartida == PART_EMPATE){
-                printf("La partida termino en empate");
-            }
-            ///Finalizo la partida.
-            finalizar_partida(&part);
+        }
+    }
+    ///Muestro el resultado de la partida.
+    if(estadoPartida == PART_GANA_JUGADOR_1){
+        printf("Gano %s!!",nombreJugador1);
+    }
+    if(estadoPartida == PART_GANA_JUGADOR_2){
+        printf("Gano %s!!",nombreJugador2);
+    }
+    if(estadoPartida == PART_EMPATE){
+        printf("La partida termino en empate");
+    }
+    ///Finalizo la partida.
+    finalizar_partida(&part);
 
     return 0;
 }
-
+///metodo que imprime las columnas de los extremos segun corresponda
 void printPositionLateral(tTablero tablero,int fila,int columna){
     if(tablero->grilla[fila][columna]==PART_SIN_MOVIMIENTO)
        printf("|   |");
@@ -217,7 +240,7 @@ void printPositionLateral(tTablero tablero,int fila,int columna){
     if(tablero->grilla[fila][columna]==PART_JUGADOR_2)
         printf("| O |");
 }
-
+///metodo que imprime la columna del medio segun corresponda
 void PrintPositionMid(tTablero tablero,int fila,int columna){
     if(tablero->grilla[fila][columna]==PART_SIN_MOVIMIENTO)
        printf("   ");
@@ -226,7 +249,7 @@ void PrintPositionMid(tTablero tablero,int fila,int columna){
     if(tablero->grilla[fila][columna]==PART_JUGADOR_2)
         printf(" O ");
 }
-
+///metodo que imprime el tablero actual
 void printActualGame(tTablero tablero){
     ///Imprimo separador.
     printf("|---|---|---|\n");
--- Proyecto-c/partida.c
+++ Proyecto-c/partida.c
@@ -3,15 +3,16 @@
 #include "stdio.h"
 #include "time.h"
 void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombre, char * j2_nombre){
-
+    ///guardo memoria para la partida
     *p=(tPartida) malloc (sizeof(struct partida));
     if((*p)==NULL)
         exit(PART_ERROR_MEMORIA);
 
-    //seteo modo y turno
+    ///seteo modo y turno
     (*p)->modo_partida=modo_partida;
-    (*p)->turno_de=comienza;       //si es jugador random que?
+    (*p)->turno_de=comienza;
     (*p)->estado=PART_EN_JUEGO;
+    ///asigno turno al azar
     if(comienza==PART_JUGADOR_RANDOM){
         srand(time(NULL));
         if(rand() % 2 == 0)
@@ -20,7 +21,7 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
             (*p)->turno_de=PART_JUGADOR_2;
     }
 
-    //seteo nombres sin chequeo
+    ///seteo nombres sin chequeo
     char guarda='\0';
     int i = 0;
     while(j1_nombre[i]!=guarda){
@@ -33,7 +34,7 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
         i++;
     }
 
-    //inicializo el tablero
+    ///inicializo el tablero con PART_SIN_MOVIMIENTO
     (*p)->tablero=(tTablero) malloc (sizeof(struct tablero));
     if((*p)->tablero == NULL)
         exit(PART_ERROR_MEMORIA);
@@ -47,57 +48,66 @@ void nueva_partida(tPartida * p, int modo_partida, int comienza, char * j1_nombr
 int ganoP(tTablero t, int ficha){
     int gano=0;
     int i=0;
+    ///chequeo filas y veo si gane
     while(gano!=3 && i<3){
         for(int j=0; j<3; j++){
             if(t->grilla[i][j]==ficha)
                 gano++;
             }
+        ///si no encontre 3 seguidos reinicio
         if(gano<3){
             gano=0;
         }
         i++;
     }
+    ///si encontre la misma ficha 3 veces en una fila gane
     if(gano>=3)
-        return 1; //gano
+        return 1;
     else
         gano=0;
 
+    ///chequeo columnas y veo si gane
     int j=0;
     while(gano!=3 && j<3){
         for(int i=0; i<3; i++){
             if(t->grilla[i][j]==ficha)
                 gano++;
             }
+        ///si no encontre 3 seguidos reinicio
         if(gano<3)
             gano=0;
         j++;
     }
+    ///si encontre la misma ficha 3 veces en una fila gane
     if(gano>=3)
         return 1; //gano
     else
         gano=0;
-
-    //diagonal hardcodeada x2
+    ///chequeo las diagonales
     i=0; j=0;
     while(j<3 && i<3){
+        ///si encuentro la ficha aumento gano
         if(t->grilla[i][j]==ficha)
             gano++;
         else{
             j=2;
             i=2;
         }
-    i++;
-    j++;
+        i++;
+        j++;
     }
+    ///si encontre la misma ficha 3 veces en una fila gane
     if(gano>=3)
         return 1; //gano
     else
         gano=0;
     i=-1;
     j=3;
+
     while(j>0 && i<3){
         j--;
         i++;
+        /// si encuentro la ficha aumento gano
         if(t->grilla[i][j]==ficha)
             gano++;
         else{
@@ -105,6 +115,7 @@ int ganoP(tTablero t, int ficha){
             i=0;
         }
     }
+    ///si encontre la misma ficha 3 veces en una fila gane
     if(gano>=3)
         return 1; //gano
     else
@@ -114,27 +125,28 @@ int ganoP(tTablero t, int ficha){
 
 int nuevo_movimiento(tPartida p, int mov_x, int mov_y){
     tTablero t=p->tablero;
-
     int lleno=0;
-
+    ///incializo el tablero vacio
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             if(t->grilla[i][j]!=PART_SIN_MOVIMIENTO)
                 lleno++;
         }
     }
-
+    ///si la posicion es fuera de la grilla ERROR
     if(mov_x > 2 || mov_y > 2 || mov_x < 0 || mov_y < 0)
         exit(PART_MOVIMIENTO_ERROR);
 
+    ///si la partida esta llena ERROR
     if(lleno>=9){
-        p->estado=PART_SIN_MOVIMIENTO;
         return PART_MOVIMIENTO_ERROR;
     }
-
+    ///si ya hay una ficha en la posicion ERROR
     if(t->grilla[mov_x][mov_y]!=PART_SIN_MOVIMIENTO)
         return (PART_MOVIMIENTO_ERROR);
+
     else{
+        ///si es el turno de Jugador 1 y gana devuelvo gana jug 1, ademas actualizo turno
         if(p->turno_de==PART_JUGADOR_1){
             t->grilla[mov_x][mov_y]=PART_JUGADOR_1;
             if(ganoP(t,PART_JUGADOR_1)){
@@ -142,6 +154,7 @@ int nuevo_movimiento(tPartida p, int mov_x, int mov_y){
             }
                 p->turno_de=PART_JUGADOR_2;
         }
+        ///si no ,chequeo si gana jugador 2 si gana devuelvo gano jug 2, ademas actualizo turno
         else{
             t->grilla[mov_x][mov_y]=PART_JUGADOR_2;
             if(ganoP(t,PART_JUGADOR_2)){
@@ -149,16 +162,21 @@ int nuevo_movimiento(tPartida p, int mov_x, int mov_y){
             }
                 p->turno_de=PART_JUGADOR_1;
         }
+        ///aumento lo lleno ya que agregue un movimiento
         lleno++;
+        ///si esta lleno se empato la partida
         if(lleno>=9){
             p->estado=PART_SIN_MOVIMIENTO;
             return PART_EMPATE;
         }
 
     }
+    ///si no paso nada de lo anterior el movimiento fue valido
     return PART_MOVIMIENTO_OK;
 }
-
+/**
+    libero toda la memoria almacenada para la partida y la destruyo
+*/
 void finalizar_partida(tPartida * p){
     free((*p)->nombre_jugador_1);
     free((*p)->nombre_jugador_2);
